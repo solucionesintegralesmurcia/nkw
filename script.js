ff9d183dd4acdf09622eff801cd789b78c30403f
solucionesintegralesmurcia <solucionesintegralesmurcia@gmail.com>
2026-04-24 22:24:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -15,3 +15,20 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
+// BOTÓN SUBIR ARRIBA
+const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    scrollTopBtn.classList.add("show");
+  } else {
+    scrollTopBtn.classList.remove("show");
+  }
+});
+
+scrollTopBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
